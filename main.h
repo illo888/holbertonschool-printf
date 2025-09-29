4637c4f3c66e87b50f0e2be358985caad8998ceb
root <root@ip-10-42-147-104.ec2.internal>
2025-09-29 20:50:39 +0000

    to come

---
+++ main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <unistd.h>
+
+/* Prototype of your _printf function */
+int _printf(const char *format, ...);
+
+/* helper functions */
+int _putchar(char c);
+int print_char(va_list args);
+int print_string(va_list args);
+int print_percent(void);
+int print_integer(va_list args);
+
+
+#endif /* MAIN_H */
