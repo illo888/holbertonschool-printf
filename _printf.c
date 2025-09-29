2b36cc94c02db94d6fb72637e7fc780a2963729d
root <root@ip-10-42-147-104.ec2.internal>
2025-09-29 20:56:30 +0000

    It mimics printf by parsing a format string with optional arguments, printing each character or formatted value accordingly

---
+++ _printf.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * _printf - Prints formatted output to stdout
+ * @format: Format string containing specifiers
+ *
+ * Return: Number of characters printed (or -1 on error)
+ */
+int _printf(const char *format, ...)
+{
+	int i = 0, count = 0;
+	va_list args;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(args, format);
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == '\0')
+			{
+				va_end(args);
+				return (-1);
+			}
+			else if (format[i] == 'c')
+				count += print_char(args);
+			else if (format[i] == 's')
+				count += print_string(args);
+			else if (format[i] == '%')
+				count += print_percent();
+			else if (format[i] == 'd' || format[i] == 'i')
+				count += print_integer(args);
+			else
+			{
+				count += _putchar('%');
+				count += _putchar(format[i]);
+			}
+		}
+		else
+			count += _putchar(format[i]);
+		i++;
+	}
+
+	va_end(args);
+	return (count);
+}
