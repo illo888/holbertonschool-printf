bf8fde0f950626b40aca82fb5dece08b4c7f5bbd
Khalid alomari <khalidalomari199@gmail.com>
2025-09-30 10:27:10 +0000

    fix main.h

--- main.h
+++ main.h
@@ -4,7 +4,7 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-/* Prototype of your _printf function */
+/* Prototype of your _printf functions */
 int _printf(const char *format, ...);
 
 /* helper functions */
