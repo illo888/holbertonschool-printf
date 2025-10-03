f2fc1d58b95e2e5693396dedcde0f4ee0b3d3b71
illo888 <shadeenn1424@gmail.com>
2025-10-03 23:57:35 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -7,42 +7,49 @@
  */
 int _printf(const char *format, ...)
 {
-	int i = 0, count = 0;
-	va_list args;
-	if (format == NULL)
-		return (-1);
-	va_start(args, format);
-	while (format[i])
-	{
-		if (format[i] == '%')
-		{
-			i++;
-			if (format[i] == '\0')
-			{
-				va_end(args);
-				return (-1);
-			}
-			else if (format[i] == 'c')
-				count += print_char(args);
-			else if (format[i] == 's')
-				count += print_string(args);
-			else if (format[i] == '%')
-				count += print_percent();
-			else if (format[i] == 'd' || format[i] == 'i')
-				count += print_integer(args);
-			else if (format[i] == 'b')
-				count += convert_binary(args);
-			else
-			{
-				count += _putchar('%');
-				count += _putchar(format[i]);
-			}
-		}
-		else
-			count += _putchar(format[i]);
-		i++;
-	}
-	va_end(args);
-	return (count);
+    int i = 0, count = 0;
+    va_list args;
+    if (format == NULL)
+        return (-1);
+    va_start(args, format);
+    while (format[i])
+    {
+        if (format[i] == '%')
+        {
+            i++;
+            if (format[i] == '\0')
+            {
+                va_end(args);
+                return (-1);
+            }
+            else if (format[i] == 'c')
+                count += print_char(args);
+            else if (format[i] == 's')
+                count += print_string(args);
+            else if (format[i] == '%')
+                count += print_percent();
+            else if (format[i] == 'd' || format[i] == 'i')
+                count += print_integer(args);
+            else if (format[i] == 'b')
+                count += convert_binary(args);
+            else if (format[i] == 'u')
+                count += unsigned_print(args);
+            else if (format[i] == 'o')
+                count += print_octal(args);
+            else if (format[i] == 'x')
+                count += print_hexadecimal(args);
+            else if (format[i] == 'X')
+                count += print_HEXADECIMAL(args);
+            else
+            {
+                count += _putchar('%');
+                count += _putchar(format[i]);
+            }
+        }
+        else
+            count += _putchar(format[i]);
+        i++;
+    }
+    va_end(args);
+    return (count);
 }
-
