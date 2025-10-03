b882638ae20cb818ea49b3a64994df517ae82369
illo888 <shadeenn1424@gmail.com>
2025-10-04 00:03:20 +0300

    Update main.h
    q3

--- main.h
+++ main.h
@@ -1,12 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdarg.h>
 #include <unistd.h>
-
 /* Prototype of your _printf functions */
 int _printf(const char *format, ...);
-
 /* helper functions */
 int _putchar(char c);
 int print_char(va_list args);
@@ -15,11 +12,15 @@ int print_percent(void);
 int print_integer(va_list args);
 int print_number(int n);
 int convert_binary(va_list args);
-int print_octal(va_list args);
 int print_rot13(va_list args);
 int print_pointer(va_list args);
 int binary(unsigned int n);
-int print_reverse(va_list args);
-
+int unsigned_print(va_list args);
+int print_unsigned_(unsigned int n);
+int print_octal(va_list args);
+int octal(unsigned int n);
+int print_hexadecimal(va_list args);
+int print_hex(unsigned int n);
+int print_HEXADECIMAL(va_list args);
+int print_HEX(unsigned int n);
 #endif /* MAIN_H */
-
