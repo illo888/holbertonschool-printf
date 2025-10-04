72bbb8c792a6e568e58ddc59723fac3c3e7368cb
illo888 <shadeenn1424@gmail.com>
2025-10-04 21:24:29 +0300

    Update more_functions.c

--- more_functions.c
+++ more_functions.c
@@ -230,3 +230,29 @@ int print_rot13(va_list args)
 
     return (count);
 }
+/**
+ * print_reverse - Prints a string in reverse
+ * @args: Argument list containing the string
+ *
+ * Return: Number of characters printed
+ */
+int print_reverse(va_list args)
+{
+    char *str = va_arg(args, char *);
+    int len = 0, count = 0;
+
+    if (str == NULL)
+        str = "(null)";
+
+    while (str[len])
+        len++;
+
+    while (len > 0)
+    {
+        _putchar(str[len - 1]);
+        count++;
+        len--;
+    }
+
+    return (count);
+}
